94571b00e4ae85e2c972ff509385fc98be79e6a3
Vladimir <merge.tool>
2017-12-13 18:07:15 +0700

    Testing

--- basket/tests.py
+++ basket/tests.py
@@ -25,13 +25,9 @@ class PostAdminTest(TestCase):
 		response = c.get('/admin/')
 		self.assertEquals(response.status_code, 302)
 
-	# Тест на создание нового пользователя
-	def test_na_novogo_usera(self):
-		self.username = 'New'
-		self.password = 'New'
-		self.user = User.objects.create_user\
-			(self.username, 'mail@example.com', self.password)
-		self.user.is_staff = False
-		self.user.is_superuser = False
-		self.user.save()
-		self.assertFalse(user.is_staff)
+	# Тест на авторизацию НЕ admin'а в админке
+	def test_na_vhod_NE_admina1(self):
+		c = Client()
+		c.login(username='test', password='test')
+		response = c.get('/admin/')
+		self.assertEquals(response.status_code, 302)
