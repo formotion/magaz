dd54ba30f470920e68719501a84bf4f82950bc91
Vladimir <merge.tool>
2017-12-15 13:35:11 +0700

    Testing

--- basket/models.py
+++ basket/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 
 class notebook(models.Model):
+    class Meta():
+        db_table = 'notebook'
+
     model = models.CharField(max_length=100,
                              verbose_name="Модель ноутбука")
     cost = models.IntegerField(verbose_name=
