979e85bccb6ab596aadaf9af6cc3907e565239d3
pewbott <34442750+pewbott@users.noreply.github.com>
2017-12-23 00:48:20 +0700

    Update tests.py

--- basket/tests.py
+++ basket/tests.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.test import TestCase, Client
 from basket.models import notebook
-from basket.models import Otlojittt
+from basket.models import Otlojit
 
 
 class PostAdminTest(TestCase):
